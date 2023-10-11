70d0ea87fb54f83e1239a57ee9e5c4e19ae4913a
kirsanovmike <kirsanovmike@gmail.com>
2023-10-11 14:52:39 +0300

    fix

--- src/pages/main/index.vue
+++ src/pages/main/index.vue
@@ -14,67 +14,12 @@
 </style>
 
 <script>
-import foote from "@/components/Footer";
 import PrivacyPolicy from "@/components/PrivacyPolicy.vue";
 
 export default {
   name: "App",
-
   components: {
-    navigation,
-    foote,
     PrivacyPolicy,
-  },
-
-  data: () => ({
-    fab: null,
-    color: "",
-    flat: null,
-  }),
-
-  computed: {
-    noDocument() {
-      return (
-        this.$route.query.document === "" || this.$route.query.document == null
-      );
-    },
-    showPrivacy() {
-      return this.$route.query.document === "privacy-policy";
-    },
-    showTerms() {
-      return this.$route.query.document === "terms-of-use";
-    },
-  },
-
-  created() {
-    const top = window.pageYOffset || 0;
-    if (top <= 60) {
-      this.color = "transparent";
-      this.flat = true;
-    }
-  },
-
-  watch: {
-    fab(value) {
-      if (value) {
-        this.color = "secondary";
-        this.flat = false;
-      } else {
-        this.color = "transparent";
-        this.flat = true;
-      }
-    },
-  },
-
-  methods: {
-    onScroll(e) {
-      if (typeof window === "undefined") return;
-      const top = window.pageYOffset || e.target.scrollTop || 0;
-      this.fab = top > 60;
-    },
-    toTop() {
-      this.$vuetify.goTo(0);
-    },
   },
 };
 </script>
